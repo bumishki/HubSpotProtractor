d75bdc56069f385bb82f350becb8266d7f3ca82a
Test User <tes@mail.com>
2019-02-17 19:55:47 +0200

    Removed some strings

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -1,17 +1,8 @@
 import {browser, $, $$, element, by, ExpectedConditions as ES, protractor, ElementFinder} from 'protractor'
 
 export class BasePage {
-    EC = protractor.ExpectedConditions;
 
     getPageTitle() {
         return browser.getTitle()
     }
-/*
-    waitPresenceOfElement(element: ElementFinder) {
-        browser.wait(this.EC.presenceOf(element), 10000, "waiter");
-    }
-
-    waitElementToBeClickable(element: ElementFinder) {
-        browser.wait(this.EC.elementToBeClickable(element), 9000, "waiter");
-    }*/
 }
